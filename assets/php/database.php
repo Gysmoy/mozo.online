54b453c0b35056722c2db5de85bd72ebe722f057
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2021-08-01 20:14:34 -0500

    database format

--- assets/php/database.php
+++ assets/php/database.php
@@ -6,15 +6,17 @@ class Database{
     private $password;
     private $charset;
     public function __construct(){
-        $this->host = 'localhost';
-        $this->db = 'mozo.online';
-        $this->user = 'root';
-        $this->password = '';
-        $this->charset = 'utf8mb4';
+        $this -> host = 'localhost';
+        $this -> db = 'mozo.online';
+        $this -> user = 'root';
+        $this -> password = '';
+        $this -> charset = 'utf8mb4';
     }
     function connect(){
         try{
-            $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
+            $connection = "mysql:host=" . $this -> host
+                        . ";dbname=" . $this -> db
+                        . ";charset=" . $this -> charset;
             $options = [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES   => false,
