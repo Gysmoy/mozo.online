96af19ea22a18afcb79af3229872ce5554641310
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2021-10-24 10:16:10 -0500

    Carlos Gamboa

--- assets/js/firestore.js
+++ assets/js/firestore.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
-import { doc, getDoc, onSnapshot, getFirestore } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
+import { collection, doc, getDoc, onSnapshot, getFirestore } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
 
 var firebaseConfig = {
     apiKey: "AIzaSyAGpDt6iCxpkSNL_5aU0SkEpD9Yw_dIixc",
@@ -15,24 +15,32 @@ const idUser = '655e786674d9d3e77bc05ed1de37b4b6bc89f788829f9f3c679e7687b410c89b
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore();
 
-onSnapshot(doc(db, 'users', idUser), (query) => {
-    if (JSON.stringify(config) === JSON.stringify(query.data().config)) { } else {
-        config = query.data().config;
-        setConfig();
-    }
-    if (JSON.stringify(menu) === JSON.stringify(query.data().menu)) { } else {
-        menu = query.data().menu;
-        setMenu();
-    }
+/* Escuchar las actualizaciones de la configuración general */
+onSnapshot(doc(db, 'users', idUser, 'config', 'general'), (general) => {
+    config.general = general.data();
+    setGeneralConfig(config.general);
+})
+
+/* Escuchar las actualizaciones de la configuración específica */
+onSnapshot(doc(db, 'users', idUser, 'config', 'especific'), (especific) => {
+    config.especific = especific.data();
+    setEspecificConfig(config.especific);
+})
+
+onSnapshot(collection(db, 'users', idUser, 'dishes'), (dishes) => {
+    dishes.forEach(contenedor => {
+        menu[contenedor.id] = contenedor.data();
+    });
+    setMenu();
 })
 
 getImage = async function getImage(idImages, idImage) {
     if (localStorage.getItem(idImage) == null) {
         console.log('Obtuvo la imagen de la base de datos');
-        const docRef = doc(db, 'images', idUser, idImages, idImage);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-            const image = docSnap.data().image;
+        const ref = doc(db, 'images', idUser, idImages, idImage);
+        const docImage = await getDoc(ref);
+        if (docImage.exists()) {
+            const image = docImage.data().data;
             localStorage.setItem(idImage, image);
             return 'data:image/jpeg;base64,' + image;
         } else {
--- assets/js/setConfig.js
+++ assets/js/setConfig.js
@@ -1,25 +1,23 @@
-var config = {};
+async function setGeneralConfig(o) {
+    $("title").text(o.pageName),
+    $("#logo").attr("alt", o.pageName);
+    var t = "";
+    t += `--background: '${await getImage('config', o.background)}';`, 
+    t += `--logo: '${await getImage('config', o.logo)}';`,
+    t += `--font: ${o.font};`,
+    $("#generalConfig").html(`:root {${t}}`);
+}
 
-function setConfig() {
-    console.log(config);
-    $('title').text(config.general.pageName);
-    $('#logo').attr('alt', config.general.pageName);
-    var root = '';
-    root += `--background: ${config.general.background};`;
-    root += `--logo: ${config.general.logo};`;
-    root += `--font: ${config.general.font};`;
-    for (const key1 in config.especific) {
-        const especific = config.especific[key1];
-        for (const key2 in especific) {
-            const value = especific[key2];
-            if (typeof value == 'object') {
-                for (const key3 in value) {
-                    root += `--${key2}-${key3}: ${value[key3]};`;
-                }
+function setEspecificConfig(o) {
+    var i = "";
+    for(const n in o)
+        for(const c in o[n])
+            if("object" == typeof o[n][c]){
+                const f = o[n][c];
+                for(const o in f)
+                    i += `--${c}-${o}: ${f[o]};`
             } else {
-                root += `--${key1}-${key2}: ${value};`;
+                i += `--${n}-${c}: ${o[n][c]};`
             }
-        }
-    }
-    $('#config').html(`:root {${root}}`);
+    $("#especificConfig").html(`:root {${i}}`);
 }
